a1771b6b36dacdca6cb0b304b06b1e0f1e46e97d
rob <you@example.com>
2022-03-23 23:08:23 -0400

    Added upsample example. Started first library function.

---
+++ examples/upsample/main.rs
@@ -0,0 +1,32 @@
+extern crate image;
+
+fn main(){
+    // Upsample factor
+    let a = 32;
+    // Set orignal image size
+    let imgx = 25;
+    let imgy = 25;
+    // Upsampled size
+    let imgxup : u32 = a * imgx;
+    let imgyup : u32 = a * imgy;
+
+    // Create a new image buffer
+    let mut imgbuf = image::ImageBuffer::new(imgx,imgy);
+    let mut imgbufup = image::ImageBuffer::new(imgxup,imgyup);
+
+    // create small red & blue image
+    for (x,y,pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (x as f32 * 255.0/imgx as f32) as u8;
+        let b = (y as f32 * 255.0/imgy as f32) as u8;
+        *pixel = image::Rgb([r,0,b]);
+    }
+
+    imgbuf.save("outputs/redblue_small.png").unwrap();
+
+    for (x,y,pixel) in imgbufup.enumerate_pixels_mut() {
+        *pixel = *(imgbuf.get_pixel((x/a) as u32, (y/a) as u32));
+    }
+
+    imgbufup.save("outputs/redblue_large_sh.png").unwrap();
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+extern crate image;
+
+
+mod interp {
+    pub fn cic (&imgbuf : image::ImageBuffer<P: Pixel, Container>, r : u32) -> image::ImageBuffer {
+        let mut buf = image::ImageBuffer::new(imgbuf.width*r as u32,imgbuf.height*r as u32);
+        for (_x,_y,pixel) in buf.enumerate_pixels_mut() {
+            *pixel = image::Rgb([0,0,0]);
+        }
+        buf
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
